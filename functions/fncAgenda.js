c35055207014edfc452fffa3e4aedb3cda24c068
Victor-Andra <cintravictor@hotmail.com>
2025-12-26 10:20:09 -0300

    530 - fix - Removendo codigo estatico

--- functions/fncAgenda.js
+++ functions/fncAgenda.js
@@ -7188,8 +7188,8 @@ module.exports = {
     // período de busca: transforma dataFil em semana (segunda..sexta) como no código original
     const baseDate = new Date(req.body.dataFil);
     
-    let dataIni = new Date(baseDate.getFullYear(), 11, 22, 0, 0, 0, 0);
-    let dataFim = new Date(2026, 0, 2, 23, 59, 59, 0);
+    let dataIni = new Date(baseDate.getFullYear(), (baseDate.getUTCMonth()), 1, 0, 0, 0, 0);
+    let dataFim = new Date(baseDate.getFullYear(), (baseDate.getUTCMonth()+1), 1, 23, 59, 59, 0);
     dataFim.setDate(dataFim.getDate()-1);
     console.log("dataIni: "+dataIni);
     console.log("dataFim: "+dataFim);
